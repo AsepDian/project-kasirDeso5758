86689fc65e462f9f5053aec4ec3c8ed09576facc
AsepDian <sepian1693@gmail.com>
2022-03-18 16:02:39 +0700

    editds

--- resources/views/kasir/dashboard.blade.php
+++ resources/views/kasir/dashboard.blade.php
@@ -1,4 +1,22 @@
 @extends('kasir.layout.main')
 @section('content')
     <h1>Ini Halaman Kasir</h1>
+    
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">Data Pembeli</div>
+            <table class="table table-bordered text-white">
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td>{{ $data->nama_pemesan }}</td>
+                </tr>
+                <tr>
+                    <td>Tgl</td>
+                    <td>:</td>
+                    <td>{{ $data->tanggal }}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @endsection
