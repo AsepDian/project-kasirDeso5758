e59f572ac1dbcbf8c242dcbebfa4a78fb33dae55
AsepDian <sepian1693@gmail.com>
2022-03-19 12:41:32 +0700

    dsupdate

--- resources/views/kasir/dashboard.blade.php
+++ resources/views/kasir/dashboard.blade.php
@@ -13,24 +13,4 @@
          </div>
 </div>
 @endforeach
-    <h1>Ini Halaman Kasir</h1>
-    
-    <div class="card">
-        <div class="card-body">
-            <div class="card-title">Data Pembeli</div>
-            <table class="table table-bordered text-white">
-                <tr>
-                    <td>Nama</td>
-                    <td>:</td>
-                    <td>{{ $data->nama_pemesan }}</td>
-                </tr>
-                <tr>
-                    <td>Tgl</td>
-                    <td>:</td>
-                    <td>{{ $data->tanggal }}</td>
-                </tr>
-            </table>
-        </div>
-    </div>
-
 @endsection
