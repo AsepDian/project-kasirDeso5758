00e96424afea1cec6c056d69c64d8043189e22a8
AsepDian <sepian1693@gmail.com>
2022-03-23 09:26:03 +0700

    menukasir

--- resources/views/kasir/dashboard.blade.php
+++ resources/views/kasir/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="col-12 col-md-4 mb-4">
         <div class="card">
             <div class="card-body" style="display: inline-block; margin:auto; text-align:center;">
-                    <img src="{{ asset('storage/'.$m->gambar ) }}" alt="" width="200" height="150"><br>
+                    <img src="{{ asset('storage/'.$m->gambar ) }}" alt="" width="200" height="150" style="border-radius: 5px;"><br>
                     <h3 style="margin-top: 5px">{{ $m->nama_menu }}</h3><br>
                     <h5>Rp. {{ $m->harga }}</h5>
                     {{-- <button type="submit" class="btn btn-primary">Pesan</button> --}}
